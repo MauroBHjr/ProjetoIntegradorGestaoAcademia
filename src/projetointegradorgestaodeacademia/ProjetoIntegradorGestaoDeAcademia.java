ec307205c0c47d280fdb86a070cdb1a7c6447400
Mauro Benck Homrich Junior <182110041@senacinformatica.br>
2022-07-01 21:44:49 -0300

    Enviando PI

---
+++ src/projetointegradorgestaodeacademia/ProjetoIntegradorGestaoDeAcademia.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package projetointegradorgestaodeacademia;
+
+/**
+ *
+ * @author 182110041
+ */
+public class ProjetoIntegradorGestaoDeAcademia {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
